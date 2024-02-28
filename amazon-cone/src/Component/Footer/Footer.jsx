b1bdc5570f88fbb98e5bf9b666ce696936fb05aa
selabrha <selabrha22@gmail.com>
2024-02-28 22:04:08 +0300

    footer

--- amazon-cone/src/Component/Footer/Footer.jsx
+++ amazon-cone/src/Component/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import classes from './Footer.module.css'
 const Footer = () => {
   return (
     <div>
         <div>
-            <p>Back to Top</p>
+            <p className={classes.upper}>Back to Top</p>
         </div>
+        <div className={classes.lower}>
         <div>
-            <h2>Get To Know Us</h2>
+            <h3>Get To Know Us</h3>
             <p>Careers</p>
             <p>Blog</p>
             <p>About Amazon</p>
@@ -16,7 +17,7 @@ const Footer = () => {
             <p>Amazon Scince</p>
         </div>
         <div>
-            <h2>Make Money With Us</h2>
+            <h3>Make Money With Us</h3>
             <p>Sell products on Amazon</p>
             <p>Sell on Amazon Business</p>
             <p>Sell apps on Amazon  </p>
@@ -27,14 +28,14 @@ const Footer = () => {
             <p>See More Make Money</p>
         </div>
         <div>
-            <h2>Amazon Payment Products</h2>
+            <h3>Amazon Payment Products</h3>
             <p>Amazon Business card</p>
             <p>Shop with Points</p>
             <p>Reload Your Balance</p>
             <p>Amazon Currency Converter</p>
         </div>
         <div>
-            <h2>Let Us Help You</h2>
+            <h3>Let Us Help You</h3>
             <p>Amazon and COVID-19</p>
             <p>Your Acount</p>
             <p>Your Orders</p>
@@ -44,6 +45,149 @@ const Footer = () => {
             <p>Amazon Asistant</p>
             <p>Amazon Assistant Help</p>
         </div>
+        </div>
+        <div className={classes.wrapper}>
+            <div className={classes.hey}>
+                <div>
+                    <h5>Amazon Music</h5>
+                    <p>Stream millons of songs</p>
+                </div>
+                <div>
+                    <h5>AmazonGlobal</h5>
+                    <p>Ship orders internationally</p>
+                </div>
+                <div>
+                    <h5>IMDbPro</h5>
+                    <p>Get info Entertaiment Professionals Need</p>
+                </div>
+            </div>
+
+
+            <div className={classes.hey}>
+                <div>
+                    <h5>Amazon Aids</h5>
+                    <p>Reach Customers Whenever they Spend thier time</p>
+                </div>
+                <div>
+                    <h5>Home Srvices</h5>
+                    <p>Exprianced Pros Happiness Gurantntee</p>
+                </div>
+                <div>
+                    <h5>Kindle Direct Puplishing</h5>
+                    <p>Indie Digital & Print Puplishing Made Easy</p>
+                </div>
+                <div>
+                    <h5>eero WiFi</h5>
+                    <p>Stream 4K video in Every room</p>
+                </div>
+            </div>
+
+
+            <div className={classes.hey}>
+                <div>
+                    <h5>6pm</h5>
+                    <p>Score deals on fashion brands</p>
+                </div>
+                <div>
+                    <h5>Amazon Web</h5>
+                    <p>Services Scalable Could Computing Servicess</p>
+                </div>
+                <div>
+                    <h5>Prime Video Direct</h5>
+                    <p>Video Destribution Made easy</p>
+                </div>
+                <div>
+                    <h5>Blink</h5>
+                    <p>Smart Security for Every Home</p>
+                </div>
+            </div>
+
+
+            <div className={classes.hey}>
+                <div>
+                    <h5>AbeBook</h5>
+                    <p>Book , Art & collectibles</p>
+                </div>
+                <div>
+                    <h5>Audible</h5>
+                    <p>Listen to Books 7 Original audios Performance </p>
+                </div>
+                <div>
+                    <h5>Shopbop</h5>
+                    <p>Designer Fashion Brand</p>
+                </div>
+                <div>
+                    <h5>Neighbours App</h5>
+                    <p>Real Time-Crime & security</p>
+                </div>
+            </div>
+
+
+            <div className={classes.hey}>
+                <div>
+                    <h5>ACX</h5>
+                    <p>AudioBook Puplishing Made easy</p>
+                </div>
+                <div>
+                    <h5>Boxoffice Mojo</h5>
+                    <p>Find Movies Box Office Data</p>
+                </div>
+                <div>
+                    <h5>Woot</h5>
+                    <p>Deals and shenanigans</p>
+                </div>
+                <div>
+                    <h5>Amazon Subscription Boxes</h5>
+                    <p>Top Subscription Boxes-right to your Roof</p>
+                </div>
+            </div>
+
+            <div className={classes.hey}>
+                <div>
+                    <h5>Sell on Amazon </h5>
+                    <p>Start a Selling Account</p>
+                </div>
+                <div>
+                    <h5>GoodtReades</h5>
+                    <p>Book reviews & recommendations</p>
+                </div>
+                <div>
+                    <h5>Zapoos</h5>
+                    <p>Shoes & closing</p>
+                </div>
+                <div>
+                    <h5>PillPack</h5>
+                    <p>Pharmacy Simplified</p>
+                </div>
+            </div>
+
+
+            <div className={classes.hey}>
+                <div>
+                    <h5>Amazon Business</h5>
+                    <p>Every thing for your Business</p>
+                </div>
+                <div>
+                    <h5>IMDb</h5>
+                    <p>Movie,Tv& Celebrities</p>
+                </div>
+                <div>
+                    <h5>Ring</h5>
+                    <p>Smart Home security system</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div className={classes.me}>
+                <p>condition of uses</p>  
+                <p> privancy note</p> 
+                <p>Your Ads Privancy chioces</p>       
+            </div>
+            <div className={classes.last}>
+            $1996-2024,Amazon.com,inc.or its Affililates
+            </div>
+        </div>
+
     </div>
   )
 }
